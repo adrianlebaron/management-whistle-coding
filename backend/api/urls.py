52522828b46b201f37f7b7428b3940c4db7e0f54
Hector Sanchez <h.manuel2311@hotmail.com>
2023-08-29 13:49:50 -0600

    code cleanup

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='api_token_auth'),
-    path('get-users/', views.getUsers, name="users")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,1 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.response import Response
-from django.contrib.auth.models import User
 
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def getUsers(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
