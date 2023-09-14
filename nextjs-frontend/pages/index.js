a2077684d00b75e55d552de66953c6ffee9f4946
Hector Sanchez <h.manuel2311@hotmail.com>
2023-09-13 19:36:14 -0600

    manuel fixed getBlogs

--- nextjs-frontend/pages/index.js
+++ nextjs-frontend/pages/index.js
@@ -1,38 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { getAllPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Head from 'next/head';
-import Layout from '../components/layout';
-import { useAuth } from '../contexts/AuthContext';
-import Router from 'next/router';
-import Linkify from 'react-linkify';
-import { FiExternalLink } from 'react-icons/fi';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../components/layout";
+import { useAuth } from "../contexts/AuthContext";
+import Router from "next/router";
+import Linkify from "react-linkify";
+import { FiExternalLink } from "react-icons/fi";
+import { getDomains, getAllPostsData } from "@/lib/posts";
 
-export async function getStaticProps() {
-  const allPostsData = await getAllPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export async function getDomains() {
-  try {
-    const response = await fetch('http://127.0.0.1:8000/api/get-domains/');
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    const data = await response.json(); // Parse the response as JSON
-    return data; // Return the parsed JSON data
-  } catch (error) {
-    throw new Error('Failed to fetch data');
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home() {
   const { isAuthenticated, setToken } = useAuth();
   const [domains, setDomains] = useState([]);
+  const [docs, setDocs] = useState([]);
 
   useEffect(() => {
     getDomains()
@@ -40,10 +19,16 @@ export default function Home({ allPostsData }) {
       .catch((error) => console.error(error));
   }, []);
 
+  useEffect(() => {
+    getAllPostsData()
+      .then((data) => setDocs(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   const handleLogout = () => {
-    setToken('');
-    Router.push('/login'); // Redirect to the login page
-  }
+    setToken("");
+    Router.push("/login"); // Redirect to the login page
+  };
 
   return (
     <Layout>
@@ -52,7 +37,9 @@ export default function Home({ allPostsData }) {
       </Head>
       {isAuthenticated() ? (
         <>
-          <button className='auth-button' onClick={handleLogout}>Logout</button>
+          <button className="auth-button" onClick={handleLogout}>
+            Logout
+          </button>
           <br />
           <section>
             <h1>Domains</h1>
@@ -61,7 +48,12 @@ export default function Home({ allPostsData }) {
                 <li key={domain.domain_url}>
                   <Linkify
                     componentDecorator={(decoratedHref, decoratedText, key) => (
-                      <a href={decoratedHref} target="_blank" rel="noopener noreferrer" key={key}>
+                      <a
+                        href={decoratedHref}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        key={key}
+                      >
                         {decoratedText}
                         <FiExternalLink />
                       </a>
@@ -72,13 +64,12 @@ export default function Home({ allPostsData }) {
                 </li>
               ))}
             </ul>
-
           </section>
 
           <section>
             <h2>Documentation</h2>
             <ul>
-              {allPostsData.map(({ id, date, title }) => {
+              {docs.map(({ id, date, title }) => {
                 // Extract only the date part from the ISO format date string
                 const trimmedDate = date.substring(0, 10);
 
@@ -97,7 +88,7 @@ export default function Home({ allPostsData }) {
         <div>
           <span>You need to log in to access this page.</span>
           <Link href="/login">
-            <button className='auth-button'>Login</button>
+            <button className="auth-button">Login</button>
           </Link>
         </div>
       )}
